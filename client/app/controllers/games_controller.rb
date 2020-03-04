01e7ce201f9707dc8b9934dd88635eb02b85e8b0
Gonzalo Matías Rey <gonzalomatiasrey@gmail.com>
2020-03-04 17:32:51 -0300

    Use Heroku url

--- client/app/controllers/games_controller.rb
+++ client/app/controllers/games_controller.rb
@@ -1,12 +1,14 @@
 require 'rest-client'
 
+MINESWEEPER_API_URL = 'https://gonzalorey-minesweeper-api.herokuapp.com/'
+
 class GamesController < ApplicationController
   def show
     response = RestClient.post 'http://localhost:8000/game', {}
 
     parsed_body = JSON.parse(response.body)
 
-    @game = Game.new(parsed_body["board"], parsed_body["timeStarted"], parsed_body["prettyTime"])
+    @game = Game.new(parsed_body['board'], parsed_body['timeStarted'], parsed_body['prettyTime'])
 
   end
 
